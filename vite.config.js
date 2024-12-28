7583571c2e779407e693e985e691d5c334dd983b
Maximillian May <83667715+mccarthurmay@users.noreply.github.com>
2024-12-27 23:44:03 -0500

    Add feature support (react-dom,router,hash-link)

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,8 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/dumpling0/'
+  base: '/dumpling0/',
+  server: {
+    historyApiFallback: true
+  }
 })
