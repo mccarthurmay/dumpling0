4f9c4f1eb91ccdb79f57e995889286baab6a4a9f
Maximillian May <83667715+mccarthurmay@users.noreply.github.com>
2025-01-12 12:17:50 -0700

    Fix website navigation

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: '/',
   server: {
     historyApiFallback: true
-  }
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+      },
+    },
+  },
 })
